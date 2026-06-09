13dd0f926da31eb1709f46b64239fb77ffb86aca
Drake <150128459+VialsShiny@users.noreply.github.com>
2026-06-09 14:43:24 +0200

    Setup CORS (Temp.)

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
